76a197d6e481ea09ce2349065f93d24be413ce68
ntokozo-nhlangulela <nnhlangulela@jhb.dvt.co.za>
2023-02-23 14:31:53 +0200

    fix: :art: Fix for the API call.

--- plane-track.js
+++ plane-track.js
@@ -1,12 +1,19 @@
-//import { apiResult } from../api/api.js
-import { backup } from "./src/flights";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+import { flightApiCall } from "./src/api/api.js";
+//import { backup as flights } from "./src/flights";
 var listContainer = document.querySelector("#flight-list");
-console.log(listContainer);
+//console.log(listContainer);
 
 function flightHandler(coordinates) {
   console.log(coordinates);
   marker.setLatLng([coordinates.long, coordinates.lat]);
+  //setFlightLocation(coordinates.lat, coordinates.long);
+  map.flyTo([coordinates.long, coordinates.lat], 11);
 }
+
+const flights = await flightApiCall();
+console.log(flights);
 
 for (let i = 0; i < 20; i++) {
   let coordinates = {
@@ -15,11 +22,11 @@ for (let i = 0; i < 20; i++) {
   };
   const flightTable = document.createElement("tr");
   const flightTableData = document.createElement("td");
-  flightTableData.innerText = backup.states[i][1];
+  flightTableData.innerText = flights[i][1];
   const flightTableData2 = document.createElement("td");
-  flightTableData2.innerText = backup.states[i][2];
+  flightTableData2.innerText = flights[i][2];
   const flightTableData3 = document.createElement("td");
-  flightTableData3.innerText = backup.states[i][9];
+  flightTableData3.innerText = flights[i][9];
 
   flightTable.appendChild(flightTableData);
   flightTable.appendChild(flightTableData2);
@@ -27,12 +34,21 @@ for (let i = 0; i < 20; i++) {
   flightTable.className = "flex-item";
 
   listContainer.appendChild(flightTable);
-  coordinates.lat = backup.states[i][5];
-  coordinates.long = backup.states[i][6];
+  coordinates.lat = flights[i][5];
+  coordinates.long = flights[i][6];
   flightTable.addEventListener("click", () => flightHandler(coordinates));
 }
 
-var map = L.map("map").setView([28.0473, 26.2041], 2);
+let map = L.map("map").setView([28.0473, 26.2041], 2);
+
+// function handleFlyTo() {
+//   const mapRef = useRef();
+//   const { current = {} } = mapRef;
+//   const { leafLetElement: map } = current;
+//   map.flyTo(coordinates, 13, {
+//     duration: 2,
+//   });
+// }
 
 L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
   maxZoom: 19,
@@ -40,11 +56,21 @@ L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
     '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo(map);
 
-var greenIcon = L.icon({
+let greenIcon = L.icon({
   iconUrl: "black-plane.png",
   iconSize: [25, 25],
   iconAnchor: [22, 94],
   popupAnchor: [-3, -76],
 });
 
-var marker = L.marker([28.0473, 26.2041], { icon: greenIcon }).addTo(map);
+let marker = L.marker([28.0473, 26.2041], { icon: greenIcon }).addTo(map);
+
+export function resetMapLocationView() {
+  map.flyTo([-35, 23], 2);
+}
+
+export function setFlightLocation(lat = 0, lon = 0) {
+  console.log(lat, long);
+  map.flyTo([lat, lon], 10);
+  marker.setLatLng([lat, lon]);
+}
--- src/api/api.js
+++ src/api/api.js
@@ -1,19 +1,32 @@
-import { backup } from "./src/flights";
-console.log(backup.states);
+// import { backup } from "./src/flights";
+// console.log(backup.states);
 
-export let apiResult = {};
-const flightApi = "https://opensky-network.org/api/states/all";
-fetch(flightApi)
-  .then((response) => response.json())
-  .then((data) => {
-    console.log(data);
-    this.apiResult = data.states.slice(0, 20).map((item) => ({
-      countryName: item[2],
-      code: item[1],
-      latitude: item[6],
-      longitude: item[5],
-    }));
-    console.log(apiResult, "Check API");
-    return apiResult;
-  });
-console.log(apiResult);
+//export let apiResult = {};
+
+// const apiCall = "https://opensky-network.org/api/states/all";
+
+// export function getFlight() {
+//   return fetch(`${apiCall}`);
+// }
+
+// const flightApi = "https://opensky-network.org/api/states/all";
+// fetch(flightApi)
+//   .then((response) => response.json())
+//   .then((data) => {
+//     console.log(data);
+//     const apiResult = data.states.slice(0, 20);
+//     console.log(apiResult);
+//   });
+
+export async function flightApiCall() {
+  //const flightApi = "https://opensky-network.org/api/states/all";
+  const flights = await fetch(flightApi)
+    .then((response) => response.json())
+    .then((data) => {
+      //console.log(data);
+      return data.states.slice(0, 20);
+      //console.log(apiResult);
+    });
+
+  return flights;
+}
